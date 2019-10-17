012c4e1c62a52e3cb0666ccc390fa0beee0dd1a7
fatalaijon <fatalaijon@gmail.com>
2019-10-17 10:58:00 +0700

    improve fraction tests, add code coverage

--- fraction.py
+++ fraction.py
@@ -1,3 +1,5 @@
+import math
+
 class Fraction:
     """A fraction with a numerator and denominator and arithmetic operations.
 
@@ -12,25 +14,57 @@ class Fraction:
         """Initialize a new fraction with the given numerator
            and denominator (default 1).
         """
-        #TODO write this (and remove this TODO comment)
-        pass
+        if not isinstance(numerator, int):
+            raise TypeError("Numerator must be int")
+        if isinstance(denominator, bool):
+            raise TypeError("Denominator must be int")
+        gcd = math.gcd(numerator,denominator)
+        if gcd == 0:
+            # avoid division by 0
+            gcd = 1
+        if denominator < 0:
+            gcd = -gcd
+        # this both removes gcd and ensures denominator >= 0
+        self.numerator = numerator//gcd
+        self.denominator = denominator//gcd
 
-    #TODO Write the __add__ method, and remove this TODO comment.
+
+    def __str__(self):
+        """Return the string in form "a/b" when a is numerator and b is denominator.
+        (or "a" when a equals to 0 or "b" equals to 1)
+        """
+        if self.denominator == 1:
+           # show value as integer
+           return str(self.numerator)
+        return '{0}/{1}'.format(self.numerator, self.denominator)
+    
+    def __neg__(self):
+        """Return negation of a fraction"""
+        return Fraction(-self.numerator, self.denominator)
+
     def __add__(self, frac):
         """Return the sum of two fractions as a new fraction.
            Use the standard formula  a/b + c/d = (ad+bc)/(b*d)
         """
-        pass
-
-    #TODO write __mul__ and __str__.  Verify __eq__ works with your code.
-    #Optional have fun and overload other operators such as 
-    # __sub__ for f-g
-    # __gt__  for f > g
-    # __neg__ for -f (negation)
-
+        num = self.numerator * frac.denominator + frac.numerator * self.denominator
+        denom = self.denominator * frac.denominator
+        return Fraction(num, denom)
+    
+    def __sub__(self, frac):
+        """Return the difference of two fractions as a new fraction."""
+        # avoid duplicate code, use self + (-frac)
+        return self.__add__(-frac)
+    
+    def __mul__(self, frac):
+        """Return the product of two fractions as a new fraction."""
+        num = self.numerator * frac.numerator
+        denom = self.denominator * frac.denominator
+        return Fraction(num, denom)
+    
     def __eq__(self, frac):
-        """Two fractions are equal if they have the same value.
-           Fractions are stored in proper form so the internal representation
-           is unique (3/6 is same as 1/2).
+        """Test if two fractions have the same value.
+
+           Fractions are stored in proper form so the internal 
+           representation is unique (3/6 is same as 1/2).
         """
         return self.numerator == frac.numerator and self.denominator == frac.denominator
--- fraction_test.py
+++ fraction_test.py
@@ -7,6 +7,10 @@ from fraction import Fraction
 class FractionTest(unittest.TestCase):
     """Test the methods and constructor of the Fraction class. """
 
+    def setUp(self):
+        self.INFINITY = Fraction(1,0)
+        self.NEG_INFINITY = Fraction(-1,0)
+
     def test_str(self):
         f = Fraction(3, -1)
         self.assertEqual("-3", f.__str__())
@@ -25,13 +29,40 @@ class FractionTest(unittest.TestCase):
         # Constructor should provide default denominator = 1
         f = Fraction(99)
         self.assertEqual("99", f.__str__())
+        # We don't have any special representation for Infinity or NaN
+        self.assertEqual("1/0", self.INFINITY.__str__())
+        f = Fraction(0,0)
+        self.assertEqual("0/0", f.__str__())
+        
 
     # TODO Write tests for __init__, __eq__, +, *.
+
     # Here is an example, but you must add more test cases.  
     # The test requires that your __eq__ is correct.
     def test_add(self):
         # 3/4 = 2/3 + 1/12
         self.assertEqual(Fraction(3,4), Fraction(1,12)+Fraction(2,3))
+        self.assertEqual(Fraction(2), Fraction(14,6) + Fraction(-1,3))
+        ZERO = Fraction(0)
+        f = Fraction(17, 18)
+        self.assertEqual(f, f+ZERO)
+        f = -f
+        self.assertEqual(f, f+ZERO)
+
+    def test_add_extended_values(self):
+        # test if + works on extended values by shear luck
+        NAN = Fraction(0,0)
+        ZERO = Fraction(0)
+        one = Fraction(3,3)
+        self.assertEqual(self.INFINITY, ZERO+Fraction(20,0))
+        self.assertEqual(self.INFINITY, Fraction(10)+self.INFINITY)
+        self.assertEqual(self.NEG_INFINITY, Fraction(10)+self.NEG_INFINITY)
+        self.assertEqual(NAN, ZERO + NAN)
+        self.assertEqual(NAN, one + NAN)
+        # the hard case
+        self.assertEqual(NAN, NAN + self.INFINITY)
+        self.assertEqual(NAN, NAN + self.NEG_INFINITY)
+        self.assertEqual(NAN, self.INFINITY + self.NEG_INFINITY)
 
     def test_eq(self):
         f = Fraction(1,2)
@@ -40,6 +71,17 @@ class FractionTest(unittest.TestCase):
         self.assertTrue(f == g)
         self.assertTrue(f.__eq__(g))  # same thing
         self.assertFalse(f == h)
-        self.assertFalse(f.__eq__(h))
-        #TODO write more tests using other cases.
-        # Consider special values like 0, 1/0, -1/0
+        # various forms of extended values
+        NAN = Fraction(0,0)
+        ZERO = Fraction(0)
+        self.assertTrue( self.INFINITY == Fraction(8,0) )
+        self.assertTrue( self.NEG_INFINITY == Fraction(-9,0) )
+        self.assertFalse( self.NEG_INFINITY == self.INFINITY )
+        self.assertFalse( self.NEG_INFINITY == NAN )
+        self.assertFalse( self.INFINITY == NAN )
+        self.assertFalse( self.INFINITY == ZERO )
+        self.assertFalse( f == NAN )
+        self.assertFalse( ZERO == NAN )
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
